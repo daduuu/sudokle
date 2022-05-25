66dad565a4f195442aae74665c34aa6dd7b0c28c
David Du <davdu21@bergen.org>
2022-05-24 17:14:36 -0700

    completed daily leaderboard display

--- sudokle/src/App.js
+++ sudokle/src/App.js
@@ -5,29 +5,50 @@ import './App.css';
 
 class App extends Component {
   state = {
-    data: []
+    leaderBoardData: []
   };
 
   componentDidMount() {
     this.fetchData()
-        .then(res => console.log(res))
+        .then(res => this.setState({
+          leaderBoardData: res
+        }))
+        //.then(res => console.log(res))
         .catch(err => console.log(err));
   }
 
   fetchData = async () => {
     const response = await fetch('/api/sudokleQueries/getLeaderboardInfo');
     const body = response.json();
-
     return body;
   };
 
+  getRows(){
+    const rows = [];
+    for(const user of this.state.leaderBoardData){
+      rows.push(<tr key={user.userID}><td>{user.userEmail}</td><td>{user.dailyPuzzleTimedSolved}</td></tr>)
+    }
+    return rows;
+  }
+
   render() {
-    //console.log(this.state.data);
+    const rows = this.getRows();
+
     return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Data from the Backend</h1>
+            <h1>Daily Leaderboard</h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Email</th>
+                      <th>Daily Time Solve</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {rows}
+                  </tbody>
+                </table>
           </header>
         </div>
     );
