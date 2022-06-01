d66e63d347f6d8d72e104555caa86f94e5285ed9
Falcons-Royale <elliotclin@gmail.com>
2022-06-01 16:46:23 -0700

    changed sign in button to link -> sign in page

--- sudokle/src/Layout.js
+++ sudokle/src/Layout.js
@@ -1,7 +1,8 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import React, {Component} from "react";
 import jwt_decode from "jwt-decode";
 import {Navbar, NavLink, Container, Nav, Button} from "react-bootstrap";
+
 
 class Layout extends Component {
     constructor(props) {
@@ -14,57 +15,8 @@ class Layout extends Component {
         };
 
     }
-
-
-    handleCallbackResponse = (response) => {
-        console.log("Encoded token" + response.credential);
-        var userObject = jwt_decode(response.credential);
-        console.log(userObject);
-        this.setState({
-            ...this.state,
-            user: userObject,
-        });
-        //after signing in, hide the sign in button and show the other features
-        document.getElementById("signInDiv").hidden = true;
-        var list = document.getElementsByClassName("signinrequired");
-
-        for(var x = 0; x < list.length; x++){
-            list.item(x).hidden = false;
-        }
-
-    }
-
-    handleSignOut = () => {
-        this.setState({
-            ...this.state,
-            user: [],
-        })
-        //after signing out, show the sign in button again and hide the other features
-        document.getElementById("signInDiv").hidden = false;
-        var list = document.getElementsByClassName("signinrequired");
-
-        for(var x = 0; x < list.length; x++){
-            list.item(x).hidden = true;
-        }
-    }
-
-
-    componentDidMount() {
-        /* global google */
-        google.accounts.id.initialize({
-            client_id: "1097344085485-1nnqi4bhqte2ah3ek71g4325rpnojmtk.apps.googleusercontent.com",
-            callback: this.handleCallbackResponse
-        });
-
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            { theme: "", size: ""}
-        );
-
-        //google.accounts.id.prompt();
-    }
+    
     render(){
-
         return(
             <>
             <Navbar bg="dark" variant="dark">
@@ -80,7 +32,7 @@ class Layout extends Component {
                 </Container>
                 <Nav>
                     <Container id="signincontainer">
-                        <Button id="ssignInDiv" className="justify-content-end">Sign in</Button>
+                        <Nav.Link as={Link} to="/SignIn">Sign in</Nav.Link>
                     </Container>
                 </Nav>
 
