853ccd6949f6c754e8306bb0de06380cda3e3ccb
David Du <davdu21@bergen.org>
2022-06-01 17:31:28 -0700

    password api

--- src/data/sudokleQueries/addUser.sql
+++ src/data/sudokleQueries/addUser.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (userEmail, dailyPuzzleSolved, dailyPuzzleTimedSolved, puzzlesSolved, averageTimeSolvedWeek)
+INSERT INTO users (userEmail, dailyPuzzleSolved, dailyPuzzleTimedSolved, puzzlesSolved, averageTimeSolvedWeek, pass)
 VALUES
-(?, ?, ?, ?, ?)
+(?, ?, ?, ?, ?, ?)
